6906597aa7f394a4b067b28f245b263d7e3edd23
Eng Siang <teo-eng-siang@hotmail.com>
2021-07-12 12:54:18 +0800

    Login Setup

---
+++ app/Modules/Account/User/Models/User.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Modules\Account\User\Models;
+
+use Auth;
+use Schema;
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+class User extends Authenticatable implements CanResetPasswordContract
+{
+    use CanResetPassword,
+        HasApiTokens;
+
+    const INSIGHTS_TOKEN_NAME = 'Insights';
+    
+    protected $connection = 'mysql';
+
+    protected $guarded = [];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
+
+    /**
+     * Generate random string for password
+     *
+     * @param Integer $length
+     *
+     * @return string
+     */
+    public static function generateRandomPassword($length = 10)
+    {
+        $charactersLength = strlen(self::CHARACTERS);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= self::CHARACTERS[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    /**
+     * Removes access tokens.
+     *
+     * @param      string  $appName  The application name
+     *
+     * @return     void
+     */
+    public function revokeExistingTokensFor(string $appName)
+    {
+        $this->tokens()->where(
+            [
+                'name' => $appName,
+                'revoked' => false
+            ]
+        )->update(['revoked' => true]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('Account')->name('account.')->group(function () {
+    Route::post('/loginByEmail', 'Login\LoginApiController@loginByEmail')->name('login_by_email');
+    Route::post('/registerByEmail', 'User\Register\RegisterApiController@registerByEmail')->name('register');
 });
