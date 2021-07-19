37ce37e2b370aede7dbf46a1c190a6475a4eb0f3
Eng Siang <teo-eng-siang@hotmail.com>
2021-07-19 15:39:50 +0800

    Update Chat Transformer

--- app/Modules/Chat/Transformers/MessageTransformer.php
+++ app/Modules/Chat/Transformers/MessageTransformer.php
@@ -7,12 +7,20 @@ use App\Modules\Chat\Models\Message;
 
 class MessageTransformer extends TransformerAbstract
 {
+	protected $currentUserId;
+
+	public function __construct($currentUserId)
+	{
+		$this->currentUserId = $currentUserId;
+	}
+
     public function transform(Message $message)
     {
         $messageArray = [
         	'id' => $message->id,
             'username' => $message->user->name,
             'message' => $message->content,
+            'own_message' => $message->sender_id == $this->currentUserId
         ];
 
         return $messageArray;
